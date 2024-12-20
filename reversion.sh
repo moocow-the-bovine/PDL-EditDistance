894a50983e82aed42345d15a4cddfda46f622918
Bryan Jurish <moocow.bovine@gmail.com>
2024-12-20 11:32:09 +0100

    bump to v0.10

---
+++ reversion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## + requires perl-reversion from Perl::Version (debian package libperl-version-perl)
+## + example call:
+##    ./reversion.sh -bump -dryrun
+
+pmfiles=(./EditDistance.pd)
+
+exec perl-reversion "$@" "${pmfiles[@]}"
+
+
